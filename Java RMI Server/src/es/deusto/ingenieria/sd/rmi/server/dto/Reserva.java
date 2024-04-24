195f71d5d487eb3025b4f7fa6dd4a24398817804
pardddo1 <markosagote@opendeusto.es>
2024-04-24 12:31:20 +0200

    guardarReserva()

--- Java RMI Server/src/es/deusto/ingenieria/sd/rmi/server/dto/Reserva.java
+++ Java RMI Server/src/es/deusto/ingenieria/sd/rmi/server/dto/Reserva.java
@@ -1,5 +1,13 @@
 package es.deusto.ingenieria.sd.rmi.server.dto;
 
-public class Reserva{
-    
+import lombok.*;
+import javax.jdo.annotations.PersistenceCapable;
+import java.io.Serializable;
+
+@PersistenceCapable
+@AllArgsConstructor
+public class Reserva implements Serializable {
+	private static final long serialVersionUID = 227L;
+	private int id;
+
 }
