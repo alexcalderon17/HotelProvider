33cab27d3f32d05423563a71684144aa841484b4
pardddo1 <markosagote@opendeusto.es>
2024-04-25 09:45:17 +0200

    final clase 25/04

--- Java RMI Client/src/es/deusto/ingenieria/sd/rmi/client/data/Reserva.java
+++ Java RMI Client/src/es/deusto/ingenieria/sd/rmi/client/data/Reserva.java
@@ -2,29 +2,39 @@ package es.deusto.ingenieria.sd.rmi.server.dto;
 
 import java.util.Date;
 
-public class Reserva {
+import lombok.*;
+import java.io.Serializable;
+
+@AllArgsConstructor
+public class Reserva implements Serializable {
+	private static final long serialVersionUID = 227L;
 	private int id;
-	private Date fechaReserva;
-	private Date fechaEntrada;
-	private Date fechaSalida;
-	private double importe;
-	private int numViajeros;
-	private String destino;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public Date getFechaReserva() {
-		return fechaReserva;
-	}
-
-	public void setFechaReserva(Date fechaReserva) {
-		this.fechaReserva = fechaReserva;
-	}
-
 }
+/*
+ * public class Reserva {
+ * private int id;
+ * private Date fechaReserva;
+ * private Date fechaEntrada;
+ * private Date fechaSalida;
+ * private double importe;
+ * private int numViajeros;
+ * private String destino;
+ * 
+ * public int getId() {
+ * return id;
+ * }
+ * 
+ * public void setId(int id) {
+ * this.id = id;
+ * }
+ * 
+ * public Date getFechaReserva() {
+ * return fechaReserva;
+ * }
+ * 
+ * public void setFechaReserva(Date fechaReserva) {
+ * this.fechaReserva = fechaReserva;
+ * }
+ * 
+ * }
+ */
--- Java RMI Server/src/es/deusto/ingenieria/sd/rmi/server/dto/Reserva.java
+++ Java RMI Server/src/es/deusto/ingenieria/sd/rmi/server/dto/Reserva.java
@@ -1,30 +1,41 @@
 package es.deusto.ingenieria.sd.rmi.server.dto;
 
 import java.util.Date;
+import lombok.*;
+import javax.jdo.annotations.PersistenceCapable;
+import java.io.Serializable;
 
-public class Reserva {
+@PersistenceCapable
+@AllArgsConstructor
+public class Reserva implements Serializable {
+	private static final long serialVersionUID = 227L;
 	private int id;
-	private Date fechaReserva;
-	private Date fechaEntrada;
-	private Date fechaSalida;
-	private double importe;
-	private int numViajeros;
-	private String destino;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public Date getFechaReserva() {
-		return fechaReserva;
-	}
-
-	public void setFechaReserva(Date fechaReserva) {
-		this.fechaReserva = fechaReserva;
-	}
-
 }
+/*
+ * public class Reserva {
+ * private int id;
+ * private Date fechaReserva;
+ * private Date fechaEntrada;
+ * private Date fechaSalida;
+ * private double importe;
+ * private int numViajeros;
+ * private String destino;
+ * 
+ * public int getId() {
+ * return id;
+ * }
+ * 
+ * public void setId(int id) {
+ * this.id = id;
+ * }
+ * 
+ * public Date getFechaReserva() {
+ * return fechaReserva;
+ * }
+ * 
+ * public void setFechaReserva(Date fechaReserva) {
+ * this.fechaReserva = fechaReserva;
+ * }
+ * 
+ * }
+ */
