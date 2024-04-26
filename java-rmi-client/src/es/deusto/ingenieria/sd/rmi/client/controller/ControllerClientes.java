bb72a2a8030a3508ddc2a5cbfbc276a86b5d50b0
pardddo1 <markosagote@opendeusto.es>
2024-04-26 17:22:56 +0200

    DAO

---
+++ java-rmi-client/src/es/deusto/ingenieria/sd/rmi/client/controller/ControllerClientes.java
@@ -0,0 +1,39 @@
+package es.deusto.ingenieria.sd.rmi.client.controller;
+
+import java.rmi.RemoteException;
+
+//import es.deusto.ingenieria.sd.rmi.client.gui.SMSWindow;
+//import es.deusto.ingenieria.sd.rmi.client.gui.SwitchServerGUI;
+import es.deusto.ingenieria.sd.rmi.client.remote.RMIServiceLocator;
+
+public class ControllerClientes 
+{
+	
+	private RMIServiceLocator rsl = null;
+	
+	public ControllerClientes(String[] args) throws RemoteException 
+	{		
+		
+        /*SMSWindow ventana = new SMSWindow(this);
+        ventana.setVisible(true);
+
+        SwitchServerGUI ventana1 = new SwitchServerGUI(this);
+        ventana1.setVisible(true);*/
+
+        this.rsl = new RMIServiceLocator();
+        this.rsl.setService(arg[0],arg[1],arg[2]);
+	}
+
+    public List<Alojamiento> getAlojamientos(){
+        return this.rsl.getService().obtenerAlojamientos();
+
+    }
+    
+    public void exit(){
+    	System.exit(0);
+    }
+    
+    public static void main(String[] args) throws RemoteException {    	
+    	new ControllerClientes(args);
+    }
+}
---
+++ java-rmi-server/src/es/deusto/ingenieria/sd/rmi/server/dao/AlojamientoDAO.java
@@ -0,0 +1,16 @@
+public class AlojamientoDAO{
+    public void insertarAlojamiento(){
+        PersistenceManagerFactory persistentManagerFactory = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+
+        PersistenceManager persistentManager = persistentManagerFactory.getPersistenceManager();	
+        Transaction transaction = persistentManager.currentTransaction();
+        
+        try{
+            transaction.begin();
+
+            
+        }
+    }
+       		
+			
+}
