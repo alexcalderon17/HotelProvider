e9559638cce35139a0c8e83c7dd637bf30b2fd8d
alexcalderon17 <114677265+alexcalderon17@users.noreply.github.com>
2024-04-27 11:01:34 +0200

    DAO a medias

--- java-rmi-server/src/es/deusto/ingenieria/sd/rmi/server/dao/AlojamientoDAO.java
+++ java-rmi-server/src/es/deusto/ingenieria/sd/rmi/server/dao/AlojamientoDAO.java
@@ -1,16 +1,28 @@
-//public class AlojamientoDAO{
-// public void insertarAlojamiento(){
-// PersistenceManagerFactory persistentManagerFactory = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+PersistenceManagerFactory persistentManagerFactory = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
 
-//PersistenceManager persistentManager = persistentManagerFactory.getPersistenceManager();	
-//Transaction transaction = persistentManager.currentTransaction();
-        
-// try{
-// transaction.begin();
+    public void insertarAlojamientoBD(String nombre, String descripcion, String direccion) {
+        PersistenceManager persistentManager = persistentManagerFactory.getPersistenceManager();
+        Transaction transaction = persistentManager.currentTransaction();
 
-            
-       // }
-   // }
-       		
-			
-//}
+        try {
+            transaction.begin();
+
+            AlojamientoDTO alojamiento = new AlojamientoDTO();
+            alojamiento.setNombre(nombre);
+            alojamiento.setDescripcion(descripcion);
+            alojamiento.setDireccion(direccion);
+
+            persistentManager.makePersistent(alojamiento);
+
+            System.out.println("+ Inserted alojamiento into db: " + alojamiento.getNombre);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            System.err.println("DBException");
+        } finally {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            persistentManager.close();
+        }
---
+++ java-rmi-server/src/es/deusto/ingenieria/sd/rmi/server/dao/ReservaDAO.java
@@ -0,0 +1,97 @@
+package es.deusto.ingenieria.sd.rmi.server.dao;
+
+import es.deusto.ingenieria.sd.rmi.server.dto.Reserva;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
+import javax.jdo.Query;
+import java.util.List;
+import java.util.Date;
+
+public class ReservaDAO {
+
+    PersistenceManagerFactory persistentManagerFactory = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+
+    public void insertarReserva(String clienteID, String alojamiento, Date fechaInicio, Date fechaFin) {
+        PersistenceManager pm = persistentManagerFactory.getPersistenceManager();
+        Transaction tx = pm.currentTransaction();
+        try {
+            tx.begin();
+            Reserva reserva = new Reserva();  // Creación del objeto sin inicializar los atributos en el constructor
+            reserva.setClienteID(clienteID);
+            reserva.setAlojamiento(alojamiento);
+            reserva.setFechaInicio(fechaInicio);
+            reserva.setFechaFin(fechaFin);
+            reserva.setEstaCancelada(false);  // Asumiendo que una nueva reserva no está cancelada por defecto
+
+            pm.makePersistent(reserva);
+            System.out.println("+ Inserted reserva into db: " + reserva.getReservaID());
+            tx.commit();
+        } catch (Exception e) {
+            System.err.println("DBException: " + e.getMessage());
+        } finally {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            pm.close();
+        }
+    }
+
+    //  public Reserva selectReserva(String reservaID) {
+    //    PersistenceManager pm = pmf.getPersistenceManager();
+    //    try {
+     //       return pm.getObjectById(Reserva.class, reservaID);
+     //   } finally {
+     //       pm.close();
+     //   }
+    }
+
+    public List
+
+
+    public void actualizarReserva(String reservaID, String clienteID, String alojamiento, Date fechaInicio, Date fechaFin, boolean estaCancelada) {
+            PersistenceManager pm = pmf.getPersistenceManager();
+            Transaction tx = pm.currentTransaction();
+            try {
+                tx.begin();
+                Reserva reserva = pm.getObjectById(Reserva.class, reservaID);
+                if (reserva != null) {
+                    reserva.setClienteID(clienteID);
+                    reserva.setAlojamiento(alojamiento);
+                    reserva.setFechaInicio(fechaInicio);
+                    reserva.setFechaFin(fechaFin);
+                    reserva.setEstaCancelada(estaCancelada);
+                    pm.makePersistent(reserva);
+                }
+                tx.commit();
+            } catch (Exception e) {
+                System.err.println("DBException: " + e.getMessage());
+            } finally {
+                if (tx.isActive()) {
+                    tx.rollback();
+                }
+                pm.close();
+            }
+        }
+
+        public void borrarReserva(String reservaID) {
+            PersistenceManager pm = pmf.getPersistenceManager();
+            Transaction tx = pm.currentTransaction();
+            try {
+                tx.begin();
+                Reserva reserva = pm.getObjectById(Reserva.class, reservaID);
+                if (reserva != null) {
+                     pm.deletePersistent(reserva);
+                }
+                tx.commit();
+            } catch (Exception e) {
+                System.err.println("DBException: " + e.getMessage());
+            } finally {
+                if (tx.isActive()) {
+                    tx.rollback();
+                }
+                pm.close();
+            }
+        }
+    }
